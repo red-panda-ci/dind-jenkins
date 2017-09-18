ec1ce0db24d03a2c291477b4d274d0cff8de44f9
Maurice Domínguez <maurice.dominguez@gigigo.com>
2017-09-18 12:52:23 +0200

    New: Add docker-compose support

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 TAG=$1
-docker run --privileged --name jenkins -p 8080:8080 redpandaci/jenkins-dind:$TAG
+docker run \
+--privileged \
+--name jenkins \
+-e JENKINS_USER=redpanda \
+-e JENKINS_PASS=redpanda \
+-p 8080:8080 \
+redpandaci/jenkins-dind:$TAG
