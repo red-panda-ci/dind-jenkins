e722a7c9f5be5b56b110a52fefa8ac566ade516f
Pedro Amador <pedro.amador@gigigo.com>
2017-12-14 18:41:45 +0100

    Build: Fix bin/test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -4,7 +4,7 @@
 cd $(dirname "$0")/..
 TEST_FOLDER=ci-scripts/.temp/jpl-test
 
-rm -f ${TEST_FOLDER}
+rm -rf ${TEST_FOLDER}
 mkdir -p ${TEST_FOLDER}
 cd ${TEST_FOLDER}
 git clone https://github.com/red-panda-ci/jenkins-pipeline-library -b master .
